63eb7f5d6536e66fb92482acf23362a6ecbb8ee8
lbarret4 <llewellynbar4@gmail.com>
2018-09-02 05:42:16 -0400

    combine event handlers for keypress and keyup

--- scripts.js
+++ scripts.js
@@ -1,32 +1,33 @@
 $(document).ready(function () {
     let $UpperKeyboard = $('#keyboard-upper-container');
     let $LowerKeyboard = $('#keyboard-lower-container');
-   $UpperKeyboard.hide();
-
-
-   $('body').on('keydown.Shift',function(e){
-    if (e.defaultPrevented) {
-    return;
-    }
-    $UpperKeyboard.show();
-    $LowerKeyboard.hide()
-
-    e.preventDefault();
-});
-
-
-$('body').on('keyup.Shift',function(){ 
     $UpperKeyboard.hide();
-    $LowerKeyboard.show();
 
-});
 
-$('body').on('keypress keyup',function(e){
-    let UtfCode= e.key.codePointAt();
-    $(`#${UtfCode}`).toggleClass('highlight');
-             
-    
-});
+    $('body').on('keypress keyup', function (e) {
+        if (e.key != 'Shift') {
+            let UtfCode = e.key.codePointAt();
+            $(`#${UtfCode}`).toggleClass('highlight');
+
+        } else if (e.key == 'Shift' && e.type == 'keyup') {
+            $UpperKeyboard.hide();
+            $LowerKeyboard.show();
+        }
+
+    });
+    $('body').on('keydown.Shift', function (e) {
+        if (e.key == 'Shift' && e.type == 'keydown') {
+
+            if (e.defaultPrevented) {
+                return;
+            }
+            $UpperKeyboard.show();
+            $LowerKeyboard.hide()
+
+            e.preventDefault();
+        }
+
+    });
 
 
 
