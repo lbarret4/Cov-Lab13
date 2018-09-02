dd1730bfd0cbd72905a0f9e532ffe27c1172b600
lbarret4 <llewellynbar4@gmail.com>
2018-09-02 05:32:44 -0400

    highlight and change keybords indiviual handlers

--- scripts.js
+++ scripts.js
@@ -1,19 +1,32 @@
 $(document).ready(function () {
     let $UpperKeyboard = $('#keyboard-upper-container');
     let $LowerKeyboard = $('#keyboard-lower-container');
-    toggleVisibility($UpperKeyboard);
+   $UpperKeyboard.hide();
 
 
-
-
-
-
-
-
-    function toggleVisibility($jObject) {
-        $jObject.toggleClass('collapse');
+   $('body').on('keydown.Shift',function(e){
+    if (e.defaultPrevented) {
+    return;
     }
+    $UpperKeyboard.show();
+    $LowerKeyboard.hide()
 
+    e.preventDefault();
+});
+
+
+$('body').on('keyup.Shift',function(){ 
+    $UpperKeyboard.hide();
+    $LowerKeyboard.show();
+
+});
+
+$('body').on('keypress keyup',function(e){
+    let UtfCode= e.key.codePointAt();
+    $(`#${UtfCode}`).toggleClass('highlight');
+             
+    
+});
 
 
 
